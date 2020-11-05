b6539f4616238660ca745f24aa922be4b5b8c719
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-05 14:28:39 +0000

    Method updated

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,16 +5,19 @@ class Fizzbuzz:
         self.arg1 = arg1
         self.arg2 = arg2
 
-    def fizzbuzz_method(self, arg1, arg2):
+    def fizzbuzz_method(self):
         for i in range(100):
-            if i % arg1 == 0 and i % arg2 == 0:
-                print("FizzBuzz")
-            elif i % arg1 == 0:
-                print("Fizz")
-            elif i % arg2 == 0:
-                print("Buzz")
+            if i % self.arg1 == 0 and i % self.arg2 == 0:
+                return("FizzBuzz")
+            elif i % self.arg1 == 0:
+                return("Fizz")
+            elif i % self.arg2 == 0:
+                return("Buzz")
+            else:
+                return(i)
+            i += 1
 
 
 test = Fizzbuzz(3, 5)
 
-print(test.Fizzbuzz(3, 5))
+print(test.fizzbuzz_method())
